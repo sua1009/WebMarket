e44277f66f375c7c189009cf0d7430f3a541a1cf
sua1009 <sua100913@gmail.com>
2021-01-26 11:18:31 +0900

    업로드할 파일 변수 추가

--- src/dto/Product.java
+++ src/dto/Product.java
@@ -21,6 +21,7 @@ public class Product implements Serializable{
 	private String category;	//분류
 	private long unitsInStock;	//재고수
 	private String condition;	//신상품 or 중고품 or 재생품 
+	private String filename;
 	
 //	기본 생성자
 	public Product() {
@@ -96,6 +97,14 @@ public class Product implements Serializable{
 
 	public void setCondition(String condition) {
 		this.condition = condition;
+	}
+
+	public String getFilename() {
+		return filename;
+	}
+
+	public void setFilename(String filename) {
+		this.filename = filename;
 	}
 
 
